167ba791e1742f0d8534fc4fe0e902bbe1da0bb6
Sim <sim@snorl.ax>
2020-09-22 15:23:57 +0800

    session id implementation in login function

--- Projects/fearless/src/backend/login.go
+++ Projects/fearless/src/backend/login.go
@@ -4,15 +4,15 @@ import (
 	"database/sql"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Header.Get("X-FORWARDED-FOR"), r.RemoteAddr)
 	db, err := sql.Open("postgres", psqlInfo)
 
 	checkErr(err)
@@ -22,7 +22,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	err = json.NewDecoder(r.Body).Decode(&requestBody)
 	checkErr(err)
 
-	db, curUser, err := userLogin(db, err, &requestBody, w)
+	db, curUser, err := userLogin(db, err, &requestBody, w, r)
 	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
 		json.NewEncoder(w).Encode(map[string]string{"status": "error", "statusText": err.Error()})
@@ -31,7 +31,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func userLogin(db *sql.DB, err error, data *loginForm, w http.ResponseWriter) (*sql.DB, userDB, error) {
+func userLogin(db *sql.DB, err error, data *loginForm, w http.ResponseWriter, r *http.Request) (*sql.DB, userDB, error) {
 	theUser, err := db.Query(`SELECT nickname, password, portraituri, userid, token FROM accounts WHERE userID='` + data.UserID + `';`)
 	var currentUser userDB
 	if err != nil {
@@ -43,23 +43,41 @@ func userLogin(db *sql.DB, err error, data *loginForm, w http.ResponseWriter) (*
 		return db, currentUser, err
 	}
 	if bcrypt.CompareHashAndPassword([]byte(currentUser.Password), []byte(data.Password)) == nil {
-		http.SetCookie(w, &http.Cookie{Name: "Nickname", Value: currentUser.Nickname, Expires: time.Now().Add(24 * time.Hour), Path: "/"})
-		http.SetCookie(w, &http.Cookie{Name: "PortraitURI", Value: currentUser.PortraitURI, Expires: time.Now().Add(24 * time.Hour), Path: "/"})
-		http.SetCookie(w, &http.Cookie{Name: "UserID", Value: currentUser.UserID, Expires: time.Now().Add(24 * time.Hour), Path: "/"})
+		db, err = createSessionTable(db, err)
+		db, session, err := addToSessionTable(db, err, r, &currentUser)
+		http.SetCookie(w, &http.Cookie{Name: "SESSIONID", Value: session, Expires: time.Now().Add(24 * time.Hour), Path: "/"})
 		return db, currentUser, err
 	}
 	return db, currentUser, errors.New("password not matched")
 }
 
-// TODO: session ID creation and store, I'll combine it with the IP
 func createSessionTable(db *sql.DB, err error) (*sql.DB, error) {
 	crt, err := db.Prepare(`CREATE TABLE IF NOT EXISTS sessions (
 		sessionID varchar(128) UNIQUE PRIMARY KEY,
-		userinDB SERIAL NOT NULL,
+		userinDB varchar(64) NOT NULL,
 		expiration date,
 		remote inet NOT NULL
 	)`)
-	checkErr(err)
+	if err != nil {
+		return db, err
+	}
 	_, err = crt.Exec()
 	return db, err
+}
+
+func addToSessionTable(db *sql.DB, err error, r *http.Request, user *userDB) (*sql.DB, string, error) {
+	newS, err := db.Prepare(`INSERT INTO sessions (sessionID, userinDB, expiration, remote) VALUES ($1, $2, $3, $4);`)
+	if err != nil {
+		return db, "", err
+	}
+	var remote string
+	if theRem := r.Header.Get("X-FORWARDED-FOR"); theRem != "" {
+		remote = theRem
+	} else {
+		remote = strings.Split(r.RemoteAddr, ":")[0]
+	}
+	userID, expiration := user.UserID, time.Now().Add(24*time.Hour)
+	session := uuid.New().String()
+	_, err = newS.Exec(session, userID, expiration, remote)
+	return db, session, err
 }
