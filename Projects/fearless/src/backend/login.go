3bdbfcd8f9f065fade9749128f4cbe5cfaa6364b
Sim <sim@snorl.ax>
2020-09-25 14:54:11 +0800

    no need in returning info from login route, since userinfo does the job

--- Projects/fearless/src/backend/login.go
+++ Projects/fearless/src/backend/login.go
@@ -25,12 +25,12 @@ func login(w http.ResponseWriter, r *http.Request) {
 	err = json.NewDecoder(r.Body).Decode(&requestBody)
 	checkErr(err)
 
-	db, curUser, err := userLogin(db, err, &requestBody, w, r)
+	db, _, err = userLogin(db, err, &requestBody, w, r)
 	w.Header().Set("Content-Type", "application/json")
 	if err != nil {
 		json.NewEncoder(w).Encode(map[string]string{"status": "error", "statusText": err.Error()})
 	} else {
-		json.NewEncoder(w).Encode(map[string]interface{}{"status": "success", "statusText": "Login successful.", "userInfo": map[string]string{"token": curUser.Token, "name": curUser.Nickname, "userID": curUser.UserID, "portraitURI": curUser.PortraitURI}})
+		json.NewEncoder(w).Encode(map[string]interface{}{"status": "success", "statusText": "Login successful."})
 	}
 }
 
