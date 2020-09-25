d79b1dd6c166c503b98c0449f4a5b704e55bf1fc
Sim <sim@snorl.ax>
2020-09-25 11:15:50 +0800

    seperate API signature and requesting feature from the functions

--- Projects/fearless/src/backend/rongAPI.go
+++ Projects/fearless/src/backend/rongAPI.go
@@ -14,28 +14,56 @@ import (
 )
 
 func registerAPI(data *userForm) userRes {
-	client := &http.Client{}
-	rand.Seed(64)
-
-	nonce, timestamp := fmt.Sprintf("%d", uint(rand.Float64()*math.Pow10(19))), fmt.Sprintf("%d", uint(time.Now().Unix()))
-	sig := fmt.Sprintf("%x", sha1.Sum([]byte(appSec+nonce+timestamp)))
 	theData := url.Values{}
 	theData.Set("userId", data.UserID)
 	theData.Set("name", data.Nickname)
 	theData.Set("portraitURI", data.PortraitURI)
-	req, _ := http.NewRequest("POST", rongURI+"/user/getToken.json", strings.NewReader(theData.Encode()))
+
+	result, err := requestRongAPI("POST", &theData, "/user/getToken.json")
+	checkErr(err)
+
+	return result
+}
+
+func changeSelfInfoAPI(data *userDB) userRes {
+	theData := url.Values{}
+	theData.Set("userId", data.UserID)
+	theData.Set("name", data.Nickname)
+	theData.Set("portraitURI", data.PortraitURI)
+
+	result, err := requestRongAPI("POST", &theData, "/user/refresh.json")
+	checkErr(err)
+
+	return result
+}
+
+func signatureUniqueRong() (string, string, string) {
+	rand.Seed(64)
+	nonce, timestamp := fmt.Sprintf("%d", uint(rand.Float64()*math.Pow10(19))), fmt.Sprintf("%d", uint(time.Now().Unix()))
+	sig := fmt.Sprintf("%x", sha1.Sum([]byte(appSec+nonce+timestamp)))
+	return nonce, timestamp, sig
+}
+
+func requestRongAPI(reqType string, data *url.Values, url string) (result userRes, err error) {
+	client := &http.Client{}
+	nonce, timestamp, sig := signatureUniqueRong()
+
+	req, _ := http.NewRequest(reqType, rongURI+url, strings.NewReader(data.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("App-Key", appKey)
 	req.Header.Set("Nonce", nonce)
 	req.Header.Set("Timestamp", timestamp)
 	req.Header.Set("Signature", sig)
+
 	resp, err := client.Do(req)
 	checkErr(err)
+
 	body, err := ioutil.ReadAll(resp.Body)
 	checkErr(err)
-	var result userRes
-	err = json.Unmarshal(body, &result)
-	checkErr(err)
 
-	return result
+	err = json.Unmarshal(body, &result)
+
+	fmt.Println("code: ", result.Code, "userID: ", result.UserID, "token: ", result.Token)
+
+	return result, err
 }
