51066d03a1b334c9a44d812dbed6420e8b1a1df7
flaribbit <1149761294@qq.com>
2020-09-27 16:58:30 +0800

    整理优化 不显示通知弹幕

--- Projects/梦飞翔队/server.js
+++ Projects/梦飞翔队/server.js
@@ -1,6 +1,6 @@
-var apikey = require('./key');
-var RongSDK = require('rongcloud-sdk')(apikey);
-var express = require('express');
+var apikey = require("./key");
+var RongSDK = require("rongcloud-sdk")(apikey);
+var express = require("express");
 var app = express();
 
 app.get("/getappkey", (_, res) => {
@@ -16,8 +16,7 @@ app.get("/exit", (req, res) => {
     onExit(req.query.token);
     res.end();
 });
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+
 app.use(express.static("static"));
 app.listen(8080, () => {
     console.log("listening 8080");
@@ -28,17 +27,17 @@ var Chatroom = Message.Chatroom;
 var User = RongSDK.User;
 
 RongSDK.Chatroom.create({
-    id: 'danmaku',
-    name: 'danmaku'
+    id: "danmaku",
+    name: "danmaku"
 });
 
 var users = [];
 for (var i = 0; i < 10; i++) {
-    var id = 'user' + i;
+    var id = "user" + i;
     User.register({
         id: id,
         name: id,
-        portrait: '.'
+        portrait: "."
     }).then(result => {
         if (result.code == 200) {
             users.push(result);
@@ -57,30 +56,42 @@ function getToken() {
     }
 }
 
+function online() {
+    var count = 0;
+    users.forEach(u => u.code == 0 ? count++ : 0);
+    return count;
+}
+
 function onExit(token) {
     var user = users.find(u => u.token == token);
     if (user) {
         console.log("exit", user.userId);
         user.code = 200;
     }
-    var count = 0;
-    users.forEach(u => u.code == 0 ? count++ : 0);
-    console.log("在线用户", count);
+    console.log("在线用户", online());
 }
 
 app.get("/test", (_, res) => {
     res.end();
     console.log("test");
+    sendMessage("admin", "这是一条来自服务端的测试消息");
+});
+
+setInterval(() => {
+    sendMessage("info", "在线人数：" + online())
+}, 5000)
+
+function sendMessage(id, text) {
     Chatroom.send({
-        senderId: 'admin',
-        targetId: 'danmaku',
-        objectName: 'RC:TxtMsg',
+        senderId: id,
+        targetId: "danmaku",
+        objectName: "RC:TxtMsg",
         content: {
-            content: 'aaaaaaaaaa'
+            content: text
         }
     }).then(result => {
         console.log(result);
     }, error => {
         console.log(error);
     });
-});
+}
--- Projects/梦飞翔队/static/index.js
+++ Projects/梦飞翔队/static/index.js
@@ -24,72 +24,74 @@ function addDanmaku(text, self, delay) {
 }
 
 var chatRoom;
-$.get('/getappkey', function (appkey) {
+$.get("/getappkey", function (appkey) {
     if (!appkey) {
-        console.log("获取appkey失败");
+        debuglog("获取appkey失败");
     }
-    console.log('获得key', appkey);
+    debuglog("获得key", appkey);
 
     var im = RongIMLib.init({ appkey: appkey });
     var conversationList = []; // 当前已存在的会话列表
     im.watch({
         conversation: function (event) {
             var updatedConversationList = event.updatedConversationList; // 更新的会话列表
-            console.log('更新会话汇总:', updatedConversationList);
-            console.log('最新会话列表:', im.Conversation.merge({
+            debuglog("更新会话汇总:", updatedConversationList);
+            debuglog("最新会话列表:", im.Conversation.merge({
                 conversationList,
                 updatedConversationList
             }));
         },
         message: function (event) {
             var message = event.message;
-            console.log('收到新消息:', message);
+            debuglog("收到新消息:", message);
             if (message.isOffLineMessage) {
-                addDanmaku(message.content.content, false, random(0, 4000));
+                if (message.senderUserId != "info") {
+                    addDanmaku(message.content.content, false, random(0, 4000));
+                }
             } else {
                 addDanmaku(message.content.content, false, 0);
             }
         },
         status: function (event) {
             var status = event.status;
-            console.log('连接状态码:', status);
+            debuglog("连接状态码:", status);
         }
     });
-    $.get('/gettoken', function (token) {
+    $.get("/gettoken", function (token) {
         im.connect({
             token: token
         }).then(function (user) {
-            console.log('链接成功, 链接用户 id 为: ', user.id);
+            debuglog("链接成功, 链接用户 id 为: ", user.id);
             //通知服务端离开
             window.onbeforeunload = function () {
                 $.get("/exit?token=" + token);
             }
             //加入聊天室
             chatRoom = im.ChatRoom.get({
-                id: 'danmaku'
+                id: "danmaku"
             });
             chatRoom.join({
                 count: 10 // 进入后, 自动拉取 10 条聊天室最新消息
             }).then(function () {
-                console.log('加入聊天室成功');
+                debuglog("加入聊天室成功");
             }).catch(function (error) {
-                console.log(error);
+                debuglog(error);
             });
         }).catch(function (error) {
-            console.log('链接失败: ', error.code, error.msg);
+            debuglog("链接失败: ", error.code, error.msg);
         });
     });
 });
 
 $("#send").click(function () {
     chatRoom.send({
-        messageType: 'RC:TxtMsg',
+        messageType: "RC:TxtMsg",
         content: {
             content: $("#text").val()
         }
     }).then(function (message) {
         addDanmaku(message.content.content, true, 0);
-        console.log('发送文字消息成功', message);
+        debuglog("发送文字消息成功", message);
     });
 });
 //api测试
@@ -99,4 +101,8 @@ $("#test").click(function () {
 
 function random(a, b) {
     return a + (b - a) * Math.random();
+}
+
+function debuglog(a, b) {
+    console.log(a, b);
 }
