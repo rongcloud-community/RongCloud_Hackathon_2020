f1e628a41aec8e31c15deb24a433d0225ac81e7f
Sim <sim@snorl.ax>
2020-10-05 21:40:35 +0800

    backend: TODO - chat and message model

---
+++ Projects/fearless/src/backend/chat.go
@@ -0,0 +1,55 @@
+// TODO: conversation model for chat messages
+// {
+// 	"unreadMessageCount": 0,
+// 	"hasMentiond": false,
+// 	"mentiondInfo": null,
+// 	"lastUnreadTime": 0,
+// 	"notificationStatus": 2,
+// 	"isTop": false,
+// 	"type": 1,
+// 	"targetId": "test2",
+// "latestMessage": {
+//   "type": 1,
+//   "targetId": "test2",
+//   "messageType": "s:person",
+//   "messageUId": "BKV3-S94Q-43C5-4JF0",
+//   "isPersited": true,
+//   "isCounted": true,
+//   "isStatusMessage": false,
+//   "senderUserId": "sim",
+//   "content": {
+// 	"content": "asdasdasdas"
+//   },
+//   "sentTime": 1601904874088,
+//   "receivedTime": 1601904873741,
+//   "messageDirection": 1,
+//   "isOffLineMessage": false,
+//   "disableNotification": false,
+//   "canIncludeExpansion": false,
+//   "expansion": null
+// },
+// 	"hasMentioned": false,
+// 	"mentionedInfo": null
+//   }
+
+// TODO: message model for chat messages
+// "latestMessage": {
+// 	"type": 1,
+// 	"targetId": "test2",
+// 	"messageType": "s:person",
+// 	"messageUId": "BKV3-S94Q-43C5-4JF0",
+// 	"isPersited": true,
+// 	"isCounted": true,
+// 	"isStatusMessage": false,
+// 	"senderUserId": "sim",
+// 	"content": {
+// 	  "content": "asdasdasdas"
+// 	},
+// 	"sentTime": 1601904874088,
+// 	"receivedTime": 1601904873741,
+// 	"messageDirection": 1,
+// 	"isOffLineMessage": false,
+// 	"disableNotification": false,
+// 	"canIncludeExpansion": false,
+// 	"expansion": null
+//   },
