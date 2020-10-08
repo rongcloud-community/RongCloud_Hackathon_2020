5331ac8e4368ded8f13d70a1cc8fe28c32a811ac
yuxuan <2153059054@qq.com>
2020-10-08 08:39:56 +0800

    网页框架

---
+++ Projects/点兔万岁/server.js
@@ -0,0 +1,14 @@
+var apikey = require("./key");
+var RongCloudSDK = require("rongcloud-sdk")(apikey);
+var express = require("express");
+var app = express();
+
+app.get("/getappkey", (_, res) => {
+    console.log("key");
+    res.end(apikey.appkey);
+});
+
+app.use(express.static("static"));
+app.listen(8080, () => {
+    console.log("listening 8080");
+});
