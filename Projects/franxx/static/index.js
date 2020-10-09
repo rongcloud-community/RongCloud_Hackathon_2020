7f19d96d694ea0fb37f2a887258b3185edf1b74f
jx <2657041838@qq.com>
2020-10-09 20:52:40 +0800

    连接实时消息

---
+++ Projects/franxx/static/index.js
@@ -0,0 +1,54 @@
+var conversation;
+$.get("/getappkey", function (appkey) {
+    if (!appkey) {
+        alert("获取appkey失败");
+    }
+    console.log(appkey);
+
+    //融云实时消息SDK
+    var im = RongIMLib.init({ appkey: appkey });
+    var conversationList = [];
+    im.watch({
+        conversation: function (event) {
+            //更新会话列表
+            var updatedConversationList = event.updatedConversationList;
+            console.log("更新会话汇总:", updatedConversationList);
+            console.log("最新会话列表:", im.Conversation.merge({
+                conversationList,
+                updatedConversationList
+            }));
+        },
+        message: function (event) {
+            //收到新消息
+            var message = event.message;
+            console.log("收到新消息:", message);
+        },
+        status: function (event) {
+            //状态改变
+            var status = event.status;
+            console.log("连接状态码:", status);
+        }
+    });
+
+    $.getJSON("/gettoken", function (token) {
+        if (token.token == "") {
+            alert("获取token失败！");
+            return;
+        }
+        console.log(token.token);
+
+        window.onbeforeunload = function () {
+            $.get("/exit?token=" + encodeURIComponent(token.token));
+            return "确认退出";
+        }
+
+        //连接实时消息
+        im.connect({
+            token: token.token
+        }).then(function (user) {
+            console.log("链接成功, 链接用户 id 为: ", user.id);
+        }).catch(function (error) {
+            alert("链接失败: " + error.code + error.msg);
+        });
+    });
+});
