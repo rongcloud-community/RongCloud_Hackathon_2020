c6d38bc7fe802c3e0e7722c30e715549e01b2591
wangyue8 [王月] <wangyue8@xiaomi.com>
2020-10-09 17:19:46 +0800

    直播 加入房间流程走通

--- Projects/秋分/FellowLive/app/src/main/java/com/wy/fellowlive/ui/HomeActivity.java
+++ Projects/秋分/FellowLive/app/src/main/java/com/wy/fellowlive/ui/HomeActivity.java
@@ -2,16 +2,24 @@ package com.wy.fellowlive.ui;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+
 import com.blankj.utilcode.constant.PermissionConstants;
 import com.blankj.utilcode.util.PermissionUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.wy.fellowlive.FellowApplication;
 import com.wy.fellowlive.R;
 import com.wy.fellowlive.bean.UserToken;
@@ -24,19 +32,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.UUID;
 
 import cn.rongcloud.rtc.api.RCRTCConfig;
 import cn.rongcloud.rtc.api.RCRTCEngine;
 import cn.rongcloud.rtc.api.RCRTCLocalUser;
 import cn.rongcloud.rtc.api.RCRTCRemoteUser;
 import cn.rongcloud.rtc.api.RCRTCRoom;
+import cn.rongcloud.rtc.api.callback.IRCRTCResultCallback;
 import cn.rongcloud.rtc.api.callback.IRCRTCResultDataCallback;
 import cn.rongcloud.rtc.api.callback.IRCRTCRoomEventsListener;
 import cn.rongcloud.rtc.api.stream.RCRTCInputStream;
 import cn.rongcloud.rtc.api.stream.RCRTCLiveInfo;
+import cn.rongcloud.rtc.api.stream.RCRTCVideoInputStream;
 import cn.rongcloud.rtc.api.stream.RCRTCVideoStreamConfig;
 import cn.rongcloud.rtc.api.stream.RCRTCVideoView;
+import cn.rongcloud.rtc.base.RCRTCMediaType;
 import cn.rongcloud.rtc.base.RCRTCParamsType;
 import cn.rongcloud.rtc.base.RCRTCRoomType;
 import cn.rongcloud.rtc.base.RTCErrorCode;
@@ -55,6 +65,10 @@ public class HomeActivity extends Activity {
     private FrameLayout mFrameLayoutLive;
     private TextView mTxtLiveUrl;
     private Button mBtnStart;
+    private Button mBtnJoin;
+    private RCRTCRoom mRcrtcRoom;
+    private boolean joninSuccess;
+    private EditText mLiveUrlEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,23 +78,106 @@ public class HomeActivity extends Activity {
         mFrameLayoutLive = findViewById(R.id.layout_live);
         mTxtLiveUrl = findViewById(R.id.txt_live_url);
         mBtnStart = findViewById(R.id.btn_start);
+        mBtnJoin = findViewById(R.id.btn_join);
+
+        mTxtLiveUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager)
+                        getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("simple text", mTxtLiveUrl.getText().toString());
+                clipboard.setPrimaryClip(clip);
+                ToastUtils.showShort("直播链接已复制到剪贴板");
+            }
+        });
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRcrtcRoom == null) {
+                    mBtnStart.setText("关闭直播");
+                    startRoom();
+                } else {
+                    mBtnStart.setText("开启直播");
+                    mRcrtcRoom.getLocalUser().unpublishDefaultStreams(new IRCRTCResultCallback() {
+                        @Override
+                        public void onSuccess() {
+                            mTxtLiveUrl.setText("");
+                            mFrameLayoutLive.removeAllViews();
+                        }
+
+                        @Override
+                        public void onFailed(RTCErrorCode rtcErrorCode) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+        mBtnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mLiveUrlEdit==null){
+                    mLiveUrlEdit = new EditText(HomeActivity.this);
+                }
+                if (joninSuccess) {
+                    String liveUrl = mLiveUrlEdit.getText().toString();
+                    exitRoom(liveUrl);
+                } else {
+                    new AlertDialog.Builder(HomeActivity.this).setView(mLiveUrlEdit).setTitle("请输入直播链接")
+                            .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).setPositiveButton("进入", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            subsribeLive(mLiveUrlEdit.getText().toString().trim());
+                        }
+                    }).create().show();
+
+                }
+            }
+        });
 
 
         String token = FellowApplication.instance.getToken();
-        if(TextUtils.isEmpty(token)){
+        if (TextUtils.isEmpty(token)) {
             getToken();
-        }else{
-            Log.d(TAG,"token = "+token);
+        } else {
+            Log.d(TAG, "token = " + token);
             requesPermissionAndConnet(token);
         }
+
+        initVideoConfig();
+        mBtnStart.setVisibility(View.GONE);
+        mBtnJoin.setVisibility(View.GONE);
+    }
+
+    private void exitRoom(String liveUrl) {
+        RCRTCEngine.getInstance().unsubscribeLiveStream(liveUrl, new IRCRTCResultCallback() {
+            @Override
+            public void onSuccess() {
+                mFrameLayoutLive.removeAllViews();
+            mBtnJoin.setText("加入直播间");
+            }
+
+            @Override
+            public void onFailed(RTCErrorCode errorCode) {
+
+            }
+        });
+
     }
 
 
-    private void requesPermissionAndConnet(String token){
-        if(PermissionUtils.isGranted(Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO,Manifest.permission.MODIFY_AUDIO_SETTINGS)){
+    private void requesPermissionAndConnet(String token) {
+        if (PermissionUtils.isGranted(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.MODIFY_AUDIO_SETTINGS)) {
             connet(token);
-        }else{
-            PermissionUtils.permission(PermissionConstants.CAMERA,PermissionConstants.MICROPHONE).callback(new PermissionUtils.SimpleCallback() {
+        } else {
+            PermissionUtils.permission(PermissionConstants.CAMERA, PermissionConstants.MICROPHONE).callback(new PermissionUtils.SimpleCallback() {
                 @Override
                 public void onGranted() {
                     connet(token);
@@ -94,7 +191,7 @@ public class HomeActivity extends Activity {
         }
     }
 
-    private void connet(String token){
+    private void connet(String token) {
         RongIMClient.connect(token, new RongIMClient.ConnectCallback() {
             @Override
             public void onDatabaseOpened(RongIMClient.DatabaseOpenStatus code) {
@@ -104,7 +201,8 @@ public class HomeActivity extends Activity {
             @Override
             public void onSuccess(String s) {
                 //TODO 服务连接成功后，可以进行音视频加入房间操作 RCRTCEngine.getInstance().joinRoom(...)
-                joinRoom();
+                mBtnStart.setVisibility(View.VISIBLE);
+                mBtnJoin.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -114,7 +212,42 @@ public class HomeActivity extends Activity {
         });
     }
 
-    private void joinRoom(){
+    private void startRoom() {
+        // 创建本地视频显示视图
+        RCRTCVideoView rongRTCVideoView = new RCRTCVideoView(getApplicationContext());
+        RCRTCEngine.getInstance().getDefaultVideoStream().setVideoView(rongRTCVideoView);
+
+//TODO 将本地视图添加至FrameLayout布局，需要开发者自行创建布局
+        mFrameLayoutLive.removeAllViews();
+        mFrameLayoutLive.addView(rongRTCVideoView);
+//开启摄像头采集视频数据
+        RCRTCEngine.getInstance().getDefaultVideoStream().startCamera(null);
+
+//根据实际场景，选择音视频直播：LIVE_AUDIO_VIDEO 或音频直播：LIVE_AUDIO
+        RCRTCRoomType rtcRoomType = RCRTCRoomType.LIVE_AUDIO_VIDEO;
+//mRoomId：最大长度 64 个字符，可包含：`A-Z`、`a-z`、`0-9`、`+`、`=`、`-`、`_`
+        String roomId = FellowApplication.instance.getUserID() + "-" + StringUtils.getRandomString(8);
+        Log.d(TAG, "roomId = " + roomId);
+        RCRTCEngine.getInstance().joinRoom(roomId, rtcRoomType, new IRCRTCResultDataCallback<RCRTCRoom>() {
+            @Override
+            public void onSuccess(RCRTCRoom rcrtcRoom) {
+                mRcrtcRoom = rcrtcRoom;
+                //注册房间事件回调
+                rcrtcRoom.registerRoomListener(new RoomEventsListener());
+                //加入房间成功后，发布默认音视频流
+                publishDefaultAVStream(rcrtcRoom.getLocalUser());
+                //加入房间成功后，如果房间中已存在用户且发布了音、视频流，就订阅远端用户发布的音视频流.
+//                subscribeAVStream(rcrtcRoom);
+            }
+
+            @Override
+            public void onFailed(RTCErrorCode errorCode) {
+                Log.d(TAG, "joinRoom errorCode = " + errorCode.getReason());
+            }
+        });
+    }
+
+    private void initVideoConfig() {
         RCRTCConfig config = RCRTCConfig.Builder.create()
                 //是否硬解码
                 .enableHardwareDecoder(true)
@@ -133,37 +266,6 @@ public class HomeActivity extends Activity {
                 .setMaxRate(900)
                 .build();
         RCRTCEngine.getInstance().getDefaultVideoStream().setVideoConfig(videoConfigBuilder);
-
-// 创建本地视频显示视图
-        RCRTCVideoView rongRTCVideoView = new RCRTCVideoView(getApplicationContext());
-        RCRTCEngine.getInstance().getDefaultVideoStream().setVideoView(rongRTCVideoView);
-
-//TODO 将本地视图添加至FrameLayout布局，需要开发者自行创建布局
-        mFrameLayoutLive.addView(rongRTCVideoView);
-//开启摄像头采集视频数据
-        RCRTCEngine.getInstance().getDefaultVideoStream().startCamera(null);
-
-//根据实际场景，选择音视频直播：LIVE_AUDIO_VIDEO 或音频直播：LIVE_AUDIO
-        RCRTCRoomType rtcRoomType = RCRTCRoomType.LIVE_AUDIO_VIDEO;
-//mRoomId：最大长度 64 个字符，可包含：`A-Z`、`a-z`、`0-9`、`+`、`=`、`-`、`_`
-        String roomId = FellowApplication.instance.getUserID()+ "-"+StringUtils.getRandomString(8);
-        Log.d(TAG,"roomId = "+roomId);
-        RCRTCEngine.getInstance().joinRoom(roomId, rtcRoomType, new IRCRTCResultDataCallback<RCRTCRoom>() {
-            @Override
-            public void onSuccess(RCRTCRoom rcrtcRoom) {
-                //注册房间事件回调
-                rcrtcRoom.registerRoomListener(new RoomEventsListener());
-                //加入房间成功后，发布默认音视频流
-                publishDefaultAVStream(rcrtcRoom.getLocalUser());
-                //加入房间成功后，如果房间中已存在用户且发布了音、视频流，就订阅远端用户发布的音视频流.
-//                subscribeAVStream(rcrtcRoom);
-            }
-
-            @Override
-            public void onFailed(RTCErrorCode errorCode) {
-                Log.d(TAG,"joinRoom errorCode = "+errorCode.getReason());
-            }
-        });
     }
 
     private void publishDefaultAVStream(RCRTCLocalUser localUser) {
@@ -172,39 +274,73 @@ public class HomeActivity extends Activity {
             public void onSuccess(RCRTCLiveInfo liveInfo) {
                 String liveUrl = liveInfo.getLiveUrl();
                 //TODO 上传liveUrl到客户自己的APP server，提供给观众端用来订阅
-                Log.d(TAG,"liveUrl = "+liveUrl);
+
+                mTxtLiveUrl.setText(liveUrl);
+                Log.d(TAG, "liveUrl = " + liveUrl);
             }
 
             @Override
             public void onFailed(final RTCErrorCode errorCode) {
-                Log.d(TAG,"publishDefaultAVStream errorCode = "+errorCode.getReason());
+                Log.d(TAG, "publishDefaultAVStream errorCode = " + errorCode.getReason());
+            }
+        });
+    }
+
+    private void subsribeLive(String liveUrl) {
+        RCRTCEngine.getInstance().subscribeLiveStream(liveUrl, RCRTCRoomType.LIVE_AUDIO_VIDEO, new IRCRTCResultDataCallback<List<RCRTCInputStream>>() {
+            @Override
+            public void onSuccess(List<RCRTCInputStream> rcrtcInputStreams) {
+                for (RCRTCInputStream inputStream : rcrtcInputStreams) {
+                    if (inputStream.getMediaType() == RCRTCMediaType.VIDEO) {
+                        //创建 RCRTCVideoView
+                        RCRTCVideoView videoView = new RCRTCVideoView(getApplicationContext());
+                        //给input stream设置用于显示视频的视图
+                        ((RCRTCVideoInputStream) inputStream).setVideoView(videoView);
+                        //将 RCRTCVideoView 添加到自己的Layout容器中
+                        mFrameLayoutLive.removeAllViews();
+                        mFrameLayoutLive.addView(videoView);
+                        joninSuccess = true;
+                        mBtnJoin.setText("退出直播间");
+                    }
+                }
+
+            }
+
+            @Override
+            public void onFailed(RTCErrorCode errorCode) {
+                //订阅直播失败，详情查看errorCode
+                ToastUtil.showLong(HomeActivity.this, "subsribeLive onFailed");
+            }
+
+            @Override
+            public void onFailed(List<RCRTCInputStream> data, RTCErrorCode errorCode) {
+                super.onFailed(data, errorCode);
             }
         });
     }
 
 
-
-    private void getToken(){
+    private void getToken() {
         mRetrofit = new Retrofit
                 .Builder()
                 .baseUrl("https://api-cn.ronghub.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        GetTokenApi getTokenApi =  mRetrofit.create(GetTokenApi.class);
-        Map<String,String> headers = new HashMap<>();
+        GetTokenApi getTokenApi = mRetrofit.create(GetTokenApi.class);
+        Map<String, String> headers = new HashMap<>();
 
         long timestamp = System.currentTimeMillis();
         int nonce = getRandomInt();
-        String signContent = Constant.APP_SECRET+nonce+""+timestamp;
+        String signContent = Constant.APP_SECRET + nonce + "" + timestamp;
         headers.put("App-Key", Constant.APP_KEY);
-        headers.put("Nonce", nonce+"");
-        headers.put("Timestamp", timestamp+"");
+        headers.put("Nonce", nonce + "");
+        headers.put("Timestamp", timestamp + "");
         headers.put("Signature", EncryptionUtils.getSha1(signContent));
-        Map<String,String> params = new HashMap<>();
-        params.put("userId",FellowApplication.instance.getUserID());
-        Log.d(TAG,"userId = "+params.get("userId"));
-        getTokenApi.getUserToken(params,headers).enqueue(new Callback<UserToken>() {
+        Map<String, String> params = new HashMap<>();
+        params.put("userId", FellowApplication.instance.getUserID());
+        Log.d(TAG, "userId = " + params.get("userId"));
+        getTokenApi.getUserToken(params, headers).enqueue(new Callback<UserToken>() {
             @Override
             public void onResponse(Call<UserToken> call, Response<UserToken> response) {
                 requesPermissionAndConnet(response.body().token);
@@ -219,7 +355,7 @@ public class HomeActivity extends Activity {
 
     }
 
-    class RoomEventsListener extends IRCRTCRoomEventsListener{
+    class RoomEventsListener extends IRCRTCRoomEventsListener {
 
         @Override
         public void onRemoteUserPublishResource(RCRTCRemoteUser rcrtcRemoteUser, List<RCRTCInputStream> list) {
@@ -262,7 +398,7 @@ public class HomeActivity extends Activity {
         }
     }
 
-    private int getRandomInt(){
+    private int getRandomInt() {
         Random random = new Random();
         return random.nextInt(Integer.MAX_VALUE);
     }
