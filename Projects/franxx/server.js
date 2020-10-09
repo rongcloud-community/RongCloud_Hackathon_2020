32e49accf9ef08a11c09f8358f456fab27d38cc2
jx <2657041838@qq.com>
2020-10-09 21:09:29 +0800

    创建投票

--- Projects/franxx/server.js
+++ Projects/franxx/server.js
@@ -7,23 +7,34 @@ var app = express();
 app.get("/getappkey", (_, res) => res.end(apikey.appkey));
 app.get("/gettoken", (_, res) => res.end(getToken()));
 app.get("/exit", (req, res) => res.end(onExit(decodeURIComponent(req.query.token))));
+app.post("/newvote", express.urlencoded({ extended: false }), (req, res) => {
+    console.log(req.body);
+    votes.push({
+        voteid: randomID(),
+        data: req.body.data,
+        options: req.body["options[]"]
+    });
+    res.end();
+});
+app.get("/getvote", (req, res) => {
+    console.log(req.query.voteid);//voteid
+    res.end();
+});
 app.use(express.static("static"));
 app.listen(8080, () => {
     console.log("listening 8080");
 });
 
+var votes = [];
 var users = [];
 for (var i = 0; i < 10; i++) {
-    var id = "anony" + i;
+    var id = "vote" + i;
     RongUser.register({
         id: id,
         name: id,
         portrait: "."
     }).then(result => {
-        console.log(result);
-        if (result.code == 200) {
-            users.push(result);
-        }
+        users.push(result);
     }, error => {
         console.log(error);
     });
@@ -50,4 +61,8 @@ function onExit(token) {
         console.log("exit", users[index].userId);
         users[index].code = 200;
     }
+}
+
+function randomID() {
+    return Math.random().toFixed(6).substr(2);
 }
