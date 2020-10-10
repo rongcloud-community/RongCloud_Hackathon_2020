56a911b0f8741e0ce2bb580915ddbfe024f6bb37
koala <481441516@qq.com>
2020-10-10 16:04:05 +0800

    匹配机制

--- Projects/biu/server.js
+++ Projects/biu/server.js
@@ -6,12 +6,15 @@ var app = express();
 
 app.get("/getappkey", (_, res) => res.end(apikey.appkey));
 app.get("/gettoken", (_, res) => res.end(getToken()));
+app.get("/exit", (req, res) => res.end(onExit(decodeURIComponent(req.query.token))));
 app.use(express.static("static"));
 app.listen(8080, () => {
     console.log("listening 8080");
 });
 
 var users = [];
+var queue = [];
+var match = [];
 for (var i = 0; i < 10; i++) {
     var id = "rong" + i;
     RongUser.register({
@@ -29,7 +32,14 @@ function getToken() {
     for (var i = 0; i < users.length; i++) {
         if (users[i].code > 0) {
             users[i].code = 0;
+            queue.push(users[i]);
             console.log(users[i].userId, "enter");
+            if (queue.length > 1) {
+                var user1 = queue.shift();
+                var user2 = queue.shift();
+                match.push([user1, user2]);
+                console.log(user1.userId, "<>", user2.userId);
+            }
             return JSON.stringify(users[i]);
         }
     }
@@ -38,4 +48,27 @@ function getToken() {
         userId: "",
         token: ""
     });
+}
+
+function onExit(token) {
+    var index = queue.findIndex(u => u.token == token);
+    if (index > -1) {
+        console.log("exit", queue[index].userId);
+        queue[index].code = 200;
+        queue.splice(index, 1);
+    } else {
+        exitMatch(token);
+    }
+}
+
+function exitMatch(token) {
+    for (var i = 0; i < match.length; i++) {
+        if (match[i][0].token == token) {
+            match[i][0].code = 200;
+            match.splice(i, 1);
+        } else if (match[i][1].token == token) {
+            match[i][1].code = 200;
+            match.splice(i, 1);
+        }
+    }
 }
