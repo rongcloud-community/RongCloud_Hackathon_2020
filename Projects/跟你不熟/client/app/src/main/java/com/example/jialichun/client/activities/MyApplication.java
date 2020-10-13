a1f51efc9dc3f7abd29afe5d5919c2ce12cd59f2
dythebs <244271172@qq.com>
2020-10-13 23:58:13 +0800

    一见如故 - 跟你不熟

---
+++ Projects/跟你不熟/client/app/src/main/java/com/example/jialichun/client/activities/MyApplication.java
@@ -0,0 +1,59 @@
+package com.example.jialichun.client.activities;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Message;
+
+/**
+ * Created by jialichun on 2018/3/19.
+ */
+
+public class MyApplication extends Application{
+    public boolean state;
+    private Context context = null;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = getApplicationContext();
+        String appKey = "82hegw5u8vh1x";
+        RongIM.init(context, appKey);
+        RongIM.setOnReceiveMessageListener(new RongIMClient.OnReceiveMessageWrapperListener() {
+            /**
+             * 接收实时或者离线消息。
+             * 注意:
+             * 1. 针对接收离线消息时，服务端会将 200 条消息打成一个包发到客户端，客户端对这包数据进行解析。
+             * 2. hasPackage 标识是否还有剩余的消息包，left 标识这包消息解析完逐条抛送给 App 层后，剩余多少条。
+             * 如何判断离线消息收完：
+             * 1. hasPackage 和 left 都为 0；
+             * 2. hasPackage 为 0 标识当前正在接收最后一包（200条）消息，left 为 0 标识最后一包的最后一条消息也已接收完毕。
+             *
+             * @param message    接收到的消息对象
+             * @param left       每个数据包数据逐条上抛后，还剩余的条数
+             * @param hasPackage 是否在服务端还存在未下发的消息包
+             * @param offline    消息是否离线消息
+             * @return 是否处理消息。 如果 App 处理了此消息，返回 true; 否则返回 false 由 SDK 处理。
+             */
+            @Override
+            public boolean onReceived(final Message message, final int left, boolean hasPackage, boolean offline) {
+                return false;
+            }
+        });
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean s) {
+        state = s;
+    }
+
+
+}
