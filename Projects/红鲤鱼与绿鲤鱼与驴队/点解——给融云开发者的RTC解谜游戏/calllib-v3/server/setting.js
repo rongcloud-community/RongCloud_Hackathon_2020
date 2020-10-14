17349f3b9da4258678388fa58a818408e20a53da
scaret <scaret.in@gmail.com>
2020-10-14 22:03:30 +0800

    rebase and submit

---
+++ Projects/红鲤鱼与绿鲤鱼与驴队/点解——给融云开发者的RTC解谜游戏/calllib-v3/server/setting.js
@@ -0,0 +1,5 @@
+module.exports = {
+  appkey: 'sfci50a7sxcgi',
+  secret: 'qiA2b2C0jy4T',
+  port: '8443'
+};
---
+++ Projects/红鲤鱼与绿鲤鱼与驴队/点解——给融云开发者的RTC解谜游戏/publisher/src/puzzles.js
@@ -0,0 +1,11 @@
+
+import rongcloud from '../assets/rongcloud_48x48.png'
+import mario from '../assets/mario_30x45.png'
+import woman from '../assets/woman_150x240.png'
+
+
+export default [
+    {answer: "融云", path: rongcloud},
+    {answer: "马里奥", path: mario},
+    {answer: "哭泣的女人", path: woman},
+];
