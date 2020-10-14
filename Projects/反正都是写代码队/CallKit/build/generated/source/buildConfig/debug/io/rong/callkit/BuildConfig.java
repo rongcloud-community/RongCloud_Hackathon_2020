7f746ebf34e124eef8675cf7bcf8cce542ae134b
kumfo <kumfo@qq.com>
2020-10-14 20:15:44 +0800

    fix 手动引入SDK，解决无法吊起通话问题

---
+++ Projects/反正都是写代码队/CallKit/build/generated/source/buildConfig/debug/io/rong/callkit/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.rong.callkit;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "io.rong.callkit";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 20200928;
+  public static final String VERSION_NAME = "4.0.3.1_hotfix/4.0.3";
+}
