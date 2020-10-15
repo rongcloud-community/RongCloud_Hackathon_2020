8f4b5edac7a85bd2800be7f36a576c58b5f782c0
rongcloud-community <geekonline@126.com>
2020-10-15 22:08:00 +0800

    merge: pr biupiu

---
+++ Projects/biupiu/blob/src/test/java/com/myblob/blob/BlobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblob.blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
