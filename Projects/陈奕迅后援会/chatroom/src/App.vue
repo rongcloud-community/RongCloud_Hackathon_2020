a6b55fdac9009ddc43a6cbbb6a98141d4d8c9f9c
rongcloud-community <geekonline@126.com>
2020-10-15 23:32:38 +0800

    merge: pr guoliccc

---
+++ Projects/陈奕迅后援会/chatroom/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <chat/>
+    </div>
+</template>
+
+<script>
+import chat from "./components/chat.vue";
+
+export default {
+    name: "app",
+    components: {
+        chat
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ Projects/陈奕迅后援会/readme.md
@@ -0,0 +1,18 @@
+# 陈奕迅后援会-chatroom
+
+使用融云即时通讯SDK实现了一个匿名在线聊天室网站
+
+## 使用技术
+* javascript
+* vue.js
+* elementui
+* RongIM
+
+## 部署实现
+```
+    $ npm run install
+    $ npm run serve
+```
+
+## 效果图
+![](images/xgt.png)
