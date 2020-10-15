068d43eaa803e3878777d4a4c4e09b86b25f7a18
rongcloud-community <geekonline@126.com>
2020-10-15 21:14:08 +0800

    merge: pr abcd62

---
+++ Projects/heze/shouza/src/main/java/com/Xiaoyu/shouza/ShouzaApplication.java
@@ -0,0 +1,13 @@
+package com.Xiaoyu.shouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShouzaApplication.class, args);
+    }
+
+}
